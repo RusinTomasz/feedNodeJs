de39f4378e71177474debc4880c34e6cd985651d
Tomasz <tomaszrusin90@gmail.com>
2021-01-20 07:13:31 +0100

    new database

--- src/util/database.js
+++ src/util/database.js
@@ -1,26 +1,17 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("feed", "root", "", {
-  host: "localhost",
-  dialect: "mariadb",
-  port: 3306,
-  define: {
-    freezeTableName: true,
-  },
-});
-
-// const sequelize = new Sequelize(
-//   "adsvps_feed",
-//   "adsvps_feed",
-//   "63zmBoGCBReKzKJjHe",
-//   {
-//     host: "localhost",
-//     dialect: "mariadb",
-//     port: 3306,
-//     define: {
-//       freezeTableName: true,
-//     },
-//   }
-// );
+const sequelize = new Sequelize(
+  "m1105_feed",
+  "m1105_feed",
+  "xakKLHGcKyj3N7q7jzCm",
+  {
+    host: "localhost",
+    dialect: "mariadb",
+    port: 3306,
+    define: {
+      freezeTableName: true,
+    },
+  }
+);
 
 module.exports = sequelize;
